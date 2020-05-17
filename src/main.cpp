5eefab960056b325cbb642eb932072442bea0001
Khai Nguyen <khaindk@gmail.com>
2020-05-17 23:25:00 +0700

    add find boost library sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <boost/date_time.hpp>
 #include <iostream>
+
+void now() {
+    using namespace boost::posix_time;
+    ptime timeLocal = second_clock::local_time();
+
+    // The following date is in ISO 8601 extended format (CCYY-MM-DD)
+    std::cout << "Now the time is " << timeLocal << std::endl;
+}
 
 int main() {
     std::cout << "Hello there, I'm main" << std::endl;
+    now();
     return 0;
 }
